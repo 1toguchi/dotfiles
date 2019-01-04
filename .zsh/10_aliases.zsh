9db4162f0f24731c3ef4af15cbb16421f2872be4
yuu <ytksy>
2019-01-05 07:47:38 +0900

    Remove files i dont need

--- .zsh/10_aliases.zsh
+++ .zsh/10_aliases.zsh
@@ -9,8 +9,11 @@ alias vv='vim $DOTFILE_DIR/.vimrc'
 alias sz='source $DOTFILE_DIR/.zshrc'
 alias ez='exec $SHELL'
 alias vza='vim $DOTFILE_DIR/.zsh/10_aliases.zsh'
+alias vt='vim $DOTFILE_DIR/.tmux.conf'
+alias st='source $DOTFILE_DIR/.tmux.conf'
 alias ls='ls --color=auto -lh'
 alias vzantigen='vim $DOTFILE_DIR/.zshrc.antigen'
+alias tmux='tmux -2'
 
 alias rm='rm -i'
 
